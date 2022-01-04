b2e8995fccb16fe7f65e687dc028301c00592747
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2022-01-04 15:17:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-var canvas = new fabric.Canvas('myCanvas');
+var canvas = new fabric.Canvas('my_canvas');
  var x= document.getElementById("myAudio");
 
 function new_image()
 {
-	
+	fabric.new_image.URL('BirthdayImage.png' function (img){
+      block_image_object.scaleToWidth(700);
+      block_image_object.scaleToHeight(501);
+      block_image_Object.set({
+      top:0,
+      left:0    
+      });
+      canvas.add(block_image_object);
+    });
+    
 	
 }
 
 function playSound(){
-	
+	x.play();
 }
